8bb12316f2c641c449a1dacb5797bde51672b373
Andrés Torres <andytorresgonz@gmail.com>
2022-04-04 12:48:14 -0400

    biografía añadida

--- SIS457LAB0102/principal.cpp
+++ SIS457LAB0102/principal.cpp
@@ -4,8 +4,9 @@ using namespace std;
 int flag;
 int main() {
 	do {
-		cout << "biografía" << "\n";
-			cout << "Ingrese 1 si desea volver a mostrar la biografía, y cualquier otro caracter para salir" "\n";
+		cout << "Mi nombre es Andrés Alejandro Torres González, tengo 19 años, nací y actualmente vivo en Sucre, Bolivia" << "\n"
+			"soy estudiante de Ingeniería en Ciencias de la Computación y actualmente curso el 3er semestre en la USFX." << "\n";
+			cout << "Ingrese 1 si desea volver a mostrar la biografía, y cualquier otro caracter para salir:" "\n";
 			cin >> flag;
 	} while (flag == 1);
 
