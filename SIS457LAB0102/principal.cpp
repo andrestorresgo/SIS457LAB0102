85f07e49642cb49560b8adf16611c62c947307d9
Andrés Torres <andytorresgonz@gmail.com>
2022-04-04 12:01:14 -0400

    arreglo de estructura

--- SIS457LAB0102/principal.cpp
+++ SIS457LAB0102/principal.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 using namespace std;
 
@@ -13,19 +12,3 @@ int main() {
 	system("pause");
 	return 0;
 }
-=======
-#include <iostream>
-using namespace std;
-
-int flag;
-int main() {
-	do {
-		cout << "biografía" << "\n";
-			cout << "Ingrese 1 si desea volver a mostrar la biografía, y cualquier otro caracter para salir" "\n";
-			cin >> flag;
-	} while (flag == 1);
-
-	system("pause");
-	return 0;
-}
->>>>>>> c031b8d4144d5a4a1b15b0cc8f38895f8197d961
